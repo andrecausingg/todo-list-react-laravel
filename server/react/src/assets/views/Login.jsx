a835dde7c2fdb90cf4726ccbe991d02f122fb161
andrecausingg <95618374+andrecausingg@users.noreply.github.com>
2023-05-17 16:53:16 +0800

    .

---
+++ server/react/src/assets/views/Login.jsx
@@ -0,0 +1,9 @@
+const Login = () => {
+  return (
+    <>
+        <div>Log In</div>
+    </>
+  )
+}
+
+export default Login
---
+++ server/react/src/router.jsx
@@ -0,0 +1,27 @@
+import {createBrowserRouter} from "react-router-dom";
+
+import Login from "./assets/views/Login";
+import Signup from "./assets/views/Signup";
+import User from "./assets/views/User";
+import NotFound from "./assets/views/NotFound";
+
+const router = createBrowserRouter([
+    {
+        path: '/login',
+        element: <Login />
+    },
+    {
+        path: '/signup',
+        element: <Signup />
+    },
+    {
+        path: '/user',
+        element: <User />
+    },
+    {
+        path: '*',
+        element: <NotFound />
+    },
+])
+
+export default router; 
