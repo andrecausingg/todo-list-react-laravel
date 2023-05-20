594edd00bc9f07711bdc4e34a3b509a8a63c9dfb
andrecausingg <95618374+andrecausingg@users.noreply.github.com>
2023-05-20 22:45:57 +0800

    .

--- server/react/src/App.jsx
+++ server/react/src/App.jsx
@@ -1,11 +1,11 @@
 // import Signup from "./assets/components/Signup"
-import TodoList from "./assets/components/TodoList"
+// import TodoList from "./assets/components/TodoList"
 
 const App = () => {
   return (
     <>
-        <TodoList />
-        {/* <Signup /> */}
+        {/* <TodoList />
+        <Signup /> */}
     </>
   )
 }
--- server/react/src/router.jsx
+++ server/react/src/router.jsx
@@ -4,8 +4,13 @@ import Login from "./assets/views/Login";
 import Signup from "./assets/views/Signup";
 import User from "./assets/views/User";
 import NotFound from "./assets/views/NotFound";
+import TodoList from "./assets/components/TodoList";
 
 const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <TodoList />
+    },
     {
         path: '/',
         element: <Login />
